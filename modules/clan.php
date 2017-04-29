862816fc2dbf53e07ecb2c7ae1a889329964805e
OriOn-86 <florian.lepelley@gmail.com>
2017-04-29 16:47:57 +0200

    fix on clan.php

--- modules/clan.php
+++ modules/clan.php
@@ -2,7 +2,6 @@
 // clan.php
 // clan evo over 30 days
 // clan distrib over leagues
-
 // db connect
 try {
         $db = new PDO($dsn, $user, $password);
@@ -10,12 +9,10 @@ try {
         echo 'Connection failed: ' . $e->getMessage();
 }
 // latest data for display
-$sql = "SELECT `daterecord`, `clanlevel`, `clanmembers`, `clanpoints` " 
+$sql = "SELECT `daterecord`, `clanlevel`, `clanmembers`, `clanpoints` "
 	. "FROM `coc_dailyClanData` "
 	. "ORDER BY `daterecord` DESC "
-	. "LIMIT 1;";
-$qry = $db->query($sql);
-while ($row = $qry->fetch(PDO::FETCH_ASSOC)) {
+	. "LIMIT 1;"; $qry = $db->query($sql); while ($row = $qry->fetch(PDO::FETCH_ASSOC)) {
 	$clanLevel = $row['clanlevel'];
 	$nbMembers = $row['clanmembers'];
 	$nbPoints = $row['clanpoints'];
@@ -33,9 +30,7 @@ $Titan = 0;
 $Legend = 0;
 $sql = "SELECT `league` "
 	. "FROM `coc_dailyData` "
-	. "WHERE `date`=$daterecord;";
-$qry = $db->query($sql);
-while ($row = $qry->fetch(PDO::FETCH_ASSOC)) {
+	. "WHERE `date`='$daterecord';"; $qry = $db->query($sql); while ($row = $qry->fetch(PDO::FETCH_ASSOC)) {
 	$league = $row['league'];
 	if ($pos = strpos($league, " ")) {
 		$league = substr($league, 0, $pos);
@@ -70,25 +65,49 @@ while ($row = $qry->fetch(PDO::FETCH_ASSOC)) {
 			break;
 	}
 }
-if ($Unranked > 0) {$data3[]="{name:Unranked, y:" . (int)($Unranked*100/$nbMembers) ."}";}
-if ($Bronze > 0) {$data3[]="{name:Bronze, y:" . (int)($Bronze*100/$nbMembers) ."}";}
-if ($Silver > 0) {$data3[]="{name:Silver, y:" . (int)($Silver*100/$nbMembers) ."}";}
-if ($Gold > 0) {$data3[]="{name:Gold, y:" . (int)($Gold*100/$nbMembers) ."}";}
-if ($Crystal > 0) {$data3[]="{name:Crystal, y:" . (int)($Crystal*100/$nbMembers) ."}";}
-if ($Master > 0) {$data3[]="{name:Master, y:" . (int)($Master*100/$nbMembers) ."}";}
-if ($Champion > 0) {$data3[]="{name:Champion, y:" . (int)($Champion*100/$nbMembers) ."}";}
-if ($Titan > 0) {$data3[]="{name:Titan, y:" . (int)($Titan*100/$nbMembers) ."}";}
-if ($Legend > 0) {$data3[]="{name:Legend, y:" . (int)($Legend*100/$nbMembers) ."}";}
-
+if ($Unranked > 0) {
+	$colors[] = "'#f4f4f4'";
+	$data3[] = "{name:'Unranked', y:" . $Unranked ."}";
+}
+if ($Bronze > 0) {
+	$colors[] = "'#916447'";
+	$data3[] = "{name:'Bronze', y:" .$Bronze ."}";
+}
+if ($Silver > 0) {
+	$colors[] = "'#a2a2a2'";
+	$data3[] = "{name:'Silver', y:" . $Silver ."}";
+}
+if ($Gold > 0) {
+	$colors[] = "'#deb74d'";
+	$data3[] = "{name:'Gold', y:" . $Gold ."}";
+}
+if ($Crystal > 0) {
+	$colors[] = "'#ae86e3'";
+	$data3[] = "{name:'Crystal', y:" . $Crystal ."}";
+}
+if ($Master > 0) {
+	$colors[] = "'#000000'";
+	$data3[] = "{name:'Master', y:" . $Master ."}";
+}
+if ($Champion > 0) {
+	$colors[] = "'#802113'";
+	$data3[] = "{name:'Champion', y:" . $Champion ."}";
+}
+if ($Titan > 0) {
+	$colors[] = "'#fddb4a'";
+	$data3[] = "{name:'Titan', y:" . $Titan ."}";
+}
+if ($Legend > 0) {
+	$colors[] = "'#54278d'";
+	$data3[] = "{name:'Legend', y:" . $Legend ."}";
+}
 // data for charts
 // chart 1 for trophy evo.
 // chart 2 for member evo.
-$sql = "SELECT `daterecord`, `clanmembers`, `clanpoints` " 
+$sql = "SELECT `daterecord`, `clanmembers`, `clanpoints` "
 	. "FROM `coc_dailyClanData` "
 	. "ORDER BY `daterecord` DESC "
-	. "LIMIT 30;";
-$qry = $db->query($sql);
-while ($row = $qry->fetch(PDO::FETCH_ASSOC)) {
+	. "LIMIT 30;"; $qry = $db->query($sql); while ($row = $qry->fetch(PDO::FETCH_ASSOC)) {
 	$daterecord = $row['daterecord'];
 	$timestamp = (strtotime($daterecord)+60*60) * 1000;
 	$clanmembers = $row['clanmembers'];
@@ -96,38 +115,35 @@ while ($row = $qry->fetch(PDO::FETCH_ASSOC)) {
 	$data1[] = "[$timestamp, $clanpoints]";
 	$data2[] = "[$timestamp, $clanmembers]";
 }
-
 // echoing the webpage
-echo "
-<h2>Knights of Hell</h2>
+echo " <h2>Knights of Hell</h2>
 </section>
 <section>
-<div class='clan_detail'>
-<p>Au dernier relevé, le clan était niveau $clanLevel, nous étions $nbMembers et le clan avait <b>$nbPoints</b> </p><img src='images/trophies.png' />
-</div>
+	<div class='clan_detail'>
+		<p>Au dernier relevé, le clan était niveau $clanLevel, nous étions $nbMembers et le clan avait <b>$nbPoints</b> </p>
+		<img src='images/trophies.png' />
+	</div>
 </section>
 <section>
-<div id='GraphTrophy'></div>
+	<div id='GraphTrophy'></div>
 </section>
 <section>
-<div id='GraphMember'></div>
+	<div id='GraphMember'></div>
 </section>
 <section>
-<div id='Distribution'></div>
+	<div id='Distribution'></div>
 </section>
-
-<script>
-var chart1 = new Highcharts.Chart({
+<script> var chart1 = new Highcharts.Chart({
 	title: {text: 'Evolution des points du clan'},
 	chart: {renderTo: 'GraphTrophy'},
 	xAxis: {type: 'datetime', tickInterval: 24 * 36e5},
 	yAxis: {
-		title: {text: 'Trophées'}, 
+		title: {text: 'Trophées'},
 		min: 0
 	},
 	legend: {enabled: false},
 	series: [{
-		name: 'Trophies', 
+		name: 'Trophies',
 		data: [", join($data1, ',') ,"]
 	}]
 });
@@ -136,25 +152,27 @@ var chart2 = new Highcharts.Chart({
 	chart: {renderTo: 'GraphMember'},
 	xAxis: {type: 'datetime', tickInterval: 24 * 36e5},
 	yAxis: {
-		allowDecimals: false, 
+		allowDecimals: false,
 		min: 0,
 		max: 50,
 		title: {text: 'Membres'}
 	},
 	legend: {enabled: false},
 	series: [{
-		name: 'Trophies', 
+		name: 'Trophies',
 		data: [", join($data2, ',') ,"]
 	}]
 });
 var chart3 = new Highcharts.Chart({
-	title: {text: 'Distribution des joueurs par leagues'}, 
+	title: {text: 'Répartition des joueurs par leagues'},
 	chart: {
+		renderTo: 'Distribution',
 		plotBackgroundColor: null,
 		plotBorderWidth: null,
 		plotShadow: false,
 		type: 'pie'
 	},
+	colors:[", join($colors, ', '), "],
 	tooltip: {pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'},
 	plotOptions: {
 		pie: {
@@ -170,7 +188,4 @@ var chart3 = new Highcharts.Chart({
 		data: [", join($data3, ','), "]
 	}]
 });
-</script>";
-
-var_dump($Unranked ,$Bronze ,$Silver ,$Gold ,$Crystal ,$Master ,$Champion ,$Titan ,$Legend, $data3);
-?>
+</script>"; ?>
